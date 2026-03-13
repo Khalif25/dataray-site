90e1d1a0d474778aad32503801a41a18b4f61035
Abdinur Khalif <117306730+Khalif25@users.noreply.github.com>
2026-03-13 16:42:39 +0300

    indexing optimization- sitemap improved

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,7 +2,6 @@ import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const base = "https://www.dataraysmart.com";
-
   return [
     {
       url: base,
